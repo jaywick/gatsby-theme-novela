0f8f47964ed393456f78eb136da7ee9a7a49b4e3
Jay Wick <dev@jaywick.xyz>
2019-10-26 14:40:51 +1100

    Show pagination only when on articles

--- theme/src/sections/home/home-list.tsx
+++ theme/src/sections/home/home-list.tsx
@@ -1,20 +1,37 @@
 import ArticlesList from './Articles.List'
+import Paginator from '@components/Navigation/Navigation.Paginator'
 import React, { useContext } from 'react'
 import { TagsList } from './Tags.List'
-import { IArticle, ITag } from '@types'
+import { IArticle, ITag, IPaginator } from '@types'
 import { ViewTabContext } from './ViewTabContext'
+import styled from '@emotion/styled'
 
 interface Props {
-    articles: IArticle[]
-    tags: ITag[]
+    pageContext: {
+        group: IArticle[]
+        additionalContext: {
+            tags: ITag[]
+        }
+    } & IPaginator
 }
 
-export const HomeList = (props: Props) => {
+export const HomeList = ({ pageContext }: Props) => {
+    const articles = pageContext.group
+    const tags = pageContext.additionalContext.tags
     const { viewTab } = useContext(ViewTabContext)
 
     return viewTab === 'articles' ? (
-        <ArticlesList articles={props.articles} />
+        <>
+            <ArticlesList articles={articles} />
+            <ArticlesPaginator show={pageContext.pageCount > 1}>
+                <Paginator {...pageContext} />
+            </ArticlesPaginator>
+        </>
     ) : (
-        <TagsList tags={props.tags} />
+        <TagsList tags={tags} />
     )
 }
+
+const ArticlesPaginator = styled.div<{ show: boolean }>`
+    ${p => p.show && `margin-top: 95px;`}
+`
--- theme/src/templates/home.template.tsx
+++ theme/src/templates/home.template.tsx
@@ -1,28 +1,27 @@
-import React from 'react'
+import React, { useContext } from 'react'
 import styled from '@emotion/styled'
 
 import Section from '@components/Section'
 import SEO from '@components/SEO'
 import Layout from '@components/Layout'
-import Paginator from '@components/Navigation/Navigation.Paginator'
 
 import ArticlesHero from '../sections/home/Articles.Hero'
 import { IWithTheme } from '@types'
 import { HomeList } from '../sections/home/home-list'
+import { ViewTabContext } from '../sections/home/ViewTabContext'
 
 function ArticlesPage({ location, pageContext }) {
-    const articles = pageContext.group
-    const tags = pageContext.additionalContext.tags
     const authors = pageContext.additionalContext.authors
+
+    const { viewTab } = useContext(ViewTabContext)
+    console.log(viewTab)
+
     return (
         <Layout>
             <SEO pathname={location.pathname} />
             <ArticlesHero authors={authors} />
             <Section narrow>
-                <HomeList articles={articles} tags={tags} />
-                <ArticlesPaginator show={pageContext.pageCount > 1}>
-                    <Paginator {...pageContext} />
-                </ArticlesPaginator>
+                <HomeList pageContext={pageContext} />
             </Section>
             <ArticlesGradient />
         </Layout>
@@ -41,8 +40,4 @@ const ArticlesGradient = styled.div<IWithTheme>`
     pointer-events: none;
     background: ${p => p.theme.colors.gradient};
     transition: ${p => p.theme.colorModeTransition};
-`
-
-const ArticlesPaginator = styled.div<{ show: boolean }>`
-    ${p => p.show && `margin-top: 95px;`}
 `
