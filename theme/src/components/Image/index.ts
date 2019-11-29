4db608ed617ba5c35fdd047ebb57991b25be2aaf
Jay Wick <dev@jaywick.xyz>
2019-11-29 17:32:14 +1100

    Removed old files from case issue

--- theme/src/components/Image/index.ts
+++
@@ -1,5 +0,0 @@
-import { Image } from './image'
-import { Placeholder } from './placeholder'
-import { Zoom } from './zoom'
-
-export { Image, Placeholder, Zoom }
--- theme/src/components/Tables/index.ts
+++
@@ -1,6 +0,0 @@
-import { Cell } from './cell'
-import { Head } from './head'
-import { HeadCell } from './head-cell'
-import { Table } from './table'
-
-export { Cell, Head, HeadCell, Table }
